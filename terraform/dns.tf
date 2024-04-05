4908576108018aedb415a86c495e79acdd84b045
Ruben Claessens <78033666+DrTuup@users.noreply.github.com>
2024-04-05 19:43:51 +0200

    overseerr dns record added (#27)

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -26,3 +26,13 @@ resource "cloudflare_record" "www_portfolio" {
   zone_id = var.cloudflare_zone_id
 }
 
+resource "cloudflare_record" "overseerr" {
+  name    = "overseerr"
+  proxied = true
+  ttl     = 1
+  type    = "CNAME"
+  value   = cloudflare_tunnel.hydro.cname
+  zone_id = var.cloudflare_zone_id
+
+}
+
