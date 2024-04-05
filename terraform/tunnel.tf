8d5f8021e13a4f2497cfa3a03f087b2302f5c162
Ruben Claessens <78033666+DrTuup@users.noreply.github.com>
2024-04-05 18:23:44 +0200

    tunnels and more

--- terraform/tunnel.tf
+++ terraform/tunnel.tf
@@ -1,5 +1,28 @@
-resource "cloudflare_tunnel" "example" {
-  account_id = "82f3291714701b7bcca7a80429e92f8c"
-  name       = "my-tunnel"
-  secret     = "AQIDBAUGBwgBAgMEBQYHCAECAwQFBgcIAQIDBAUGBwg="
+resource "random_password" "tunnel_secret" {
+  length  = 64
+  special = true
+}
+
+resource "cloudflare_tunnel" "hydro" {
+  account_id = var.cloudflare_account_id
+  name       = "example_tunnel"
+  secret     = random_password.tunnel_secret.result
+}
+
+resource "cloudflare_tunnel_config" "example_config" {
+  account_id = var.cloudflare_account_id
+  tunnel_id  = cloudflare_tunnel.hydro.id
+
+  config {
+    ingress_rule {
+      hostname = "overseerr.${var.cloudflare_zone_name}"
+      service  = "http://192.168.1.20"
+      origin_request {
+        no_tls_verify = false
+      }
+    }
+    ingress_rule {
+      service = "http_status:404"
+    }
+  }
 }
