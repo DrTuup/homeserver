d816aa57b4050bd36b3e3669ef4abdce5ca921a0
Ruben Claessens <78033666+DrTuup@users.noreply.github.com>
2024-06-25 21:09:15 +0200

    block terraform outside netherlands

---
+++ terraform/waf.tf
@@ -0,0 +1,14 @@
+resource "cloudflare_ruleset" "zone_custom_firewall" {
+  zone_id     = var.cloudflare_zone_id
+  name        = "Custom Firewall for Overseerr"
+  description = ""
+  kind        = "zone"
+  phase       = "http_request_firewall_custom"
+
+  rules {
+    action      = "block"
+    description = "Block overseerr requests from outside the Netherlands"
+    expression  = "(http.request.full_uri contains {\"overseerr.rubenclaessens.nl\"} and not ip.geoip.country in {\"NL\"})"
+    enabled     = true
+  }
+}
