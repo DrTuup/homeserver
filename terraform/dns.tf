5325e29a252168bd204505ebb936138c8bbb58db
Ruben Claessens <78033666+DrTuup@users.noreply.github.com>
2024-09-24 18:09:25 +0200

    cloudflare records

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -3,7 +3,7 @@ resource "cloudflare_record" "portfolio" {
   proxied = true
   ttl     = 1
   type    = "A"
-  content = "76.76.21.21"
+  value   = "76.76.21.21"
   zone_id = var.cloudflare_zone_id
 }
 
@@ -12,7 +12,7 @@ resource "cloudflare_record" "www_portfolio" {
   proxied = true
   ttl     = 1
   type    = "CNAME"
-  content = "rubenclaessens.nl"
+  value   = "rubenclaessens.nl"
   zone_id = var.cloudflare_zone_id
 }
 
@@ -21,7 +21,7 @@ resource "cloudflare_record" "overseerr" {
   proxied = true
   ttl     = 1
   type    = "CNAME"
-  content = cloudflare_tunnel.hydro.cname
+  value   = cloudflare_tunnel.hydro.cname
   zone_id = var.cloudflare_zone_id
 }
 
@@ -30,7 +30,7 @@ resource "cloudflare_record" "homeassistant" {
   proxied = true
   ttl     = 1
   type    = "CNAME"
-  content = cloudflare_tunnel.hydro.cname
+  value   = cloudflare_tunnel.hydro.cname
   zone_id = var.cloudflare_zone_id
 }
 
@@ -39,7 +39,7 @@ resource "cloudflare_record" "email_spoofing_prevention_1" {
   proxied = false
   ttl     = 1
   type    = "TXT"
-  content = "v=spf1 -all"
+  value   = "v=spf1 -all"
   zone_id = var.cloudflare_zone_id
 }
 
@@ -48,7 +48,7 @@ resource "cloudflare_record" "email_spoofing_prevention_2" {
   proxied = false
   ttl     = 1
   type    = "TXT"
-  content = "v=DKIM1; p="
+  value   = "v=DKIM1; p="
   zone_id = var.cloudflare_zone_id
 }
 
@@ -57,6 +57,6 @@ resource "cloudflare_record" "email_spoofing_prevention_3" {
   proxied = false
   ttl     = 1
   type    = "TXT"
-  content = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s; rua=mailto:claessensruben2812@gmail.com"
+  value   = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s; rua=mailto:claessensruben2812@gmail.com"
   zone_id = var.cloudflare_zone_id
 }
