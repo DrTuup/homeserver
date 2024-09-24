0abb23641b679aedd5ee61c1678e3ce6e6ded37a
Ruben Claessens <78033666+DrTuup@users.noreply.github.com>
2024-09-24 18:07:07 +0200

    cloudflare records

--- terraform/tunnel.tf
+++ terraform/tunnel.tf
@@ -23,6 +23,14 @@ resource "cloudflare_tunnel_config" "example_config" {
     }
 
     ingress_rule {
+      hostname = "homeassistant.${var.cloudflare_zone_name}"
+      service  = "https://192.168.1.30"
+      origin_request {
+        no_tls_verify = true
+      }
+    }
+
+    ingress_rule {
       service = "http_status:404"
     }
   }
