65e9f0c56dd96e8563e5899e7a613abfae5ae886
Ruben Claessens <78033666+DrTuup@users.noreply.github.com>
2024-09-24 18:16:06 +0200

    waf for homeassistant

--- terraform/waf.tf
+++ terraform/waf.tf
@@ -1,14 +1,14 @@
 resource "cloudflare_ruleset" "zone_custom_firewall" {
   zone_id     = var.cloudflare_zone_id
-  name        = "Custom Firewall for Overseerr"
+  name        = "Custom Firewall for Services"
   description = ""
   kind        = "zone"
   phase       = "http_request_firewall_custom"
 
   rules {
     action      = "block"
-    description = "Block overseerr requests from outside the Netherlands"
-    expression  = "(http.request.full_uri contains \"overseerr.rubenclaessens.nl\" and ip.geoip.country ne \"NL\")"
+    description = "Block requests from outside the Netherlands"
+    expression  = "(http.request.full_uri contains \"overseerr.rubenclaessens.nl\" or http.request.full_uri contains \"homeassistant.rubenclaessens.nl\") and ip.geoip.country ne \"NL\""
     enabled     = true
   }
 }
