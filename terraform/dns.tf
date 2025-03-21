85d3d3a8ce8b8323d9bb14c0265e6346cc197505
Ruben Claessens <78033666+DrTuup@users.noreply.github.com>
2025-03-21 16:24:23 +0100

    Cloudflare provider upgrade

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,62 +1,62 @@
-resource "cloudflare_record" "portfolio" {
+resource "cloudflare_dns_record" "portfolio" {
   name    = "rubenclaessens.nl"
   proxied = true
   ttl     = 1
   type    = "A"
-  value   = "76.76.21.21"
+  content = "76.76.21.21"
   zone_id = var.cloudflare_zone_id
 }
 
-resource "cloudflare_record" "www_portfolio" {
+resource "cloudflare_dns_record" "www_portfolio" {
   name    = "www"
   proxied = true
   ttl     = 1
   type    = "CNAME"
-  value   = "rubenclaessens.nl"
+  content = "rubenclaessens.nl"
   zone_id = var.cloudflare_zone_id
 }
 
-resource "cloudflare_record" "overseerr" {
+resource "cloudflare_dns_record" "overseerr" {
   name    = "overseerr"
   proxied = true
   ttl     = 1
   type    = "CNAME"
-  value   = cloudflare_tunnel.hydro.cname
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.rpi-01.id}.cfargotunnel.com"
   zone_id = var.cloudflare_zone_id
 }
 
-resource "cloudflare_record" "homeassistant" {
+resource "cloudflare_dns_record" "homeassistant" {
   name    = "homeassistant"
   proxied = true
   ttl     = 1
   type    = "CNAME"
-  value   = cloudflare_tunnel.hydro.cname
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.rpi-01.id}.cfargotunnel.com"
   zone_id = var.cloudflare_zone_id
 }
 
-resource "cloudflare_record" "email_spoofing_prevention_1" {
+resource "cloudflare_dns_record" "prevent_email_spoofing_1" {
   name    = "rubenclaessens.nl"
   proxied = false
   ttl     = 1
   type    = "TXT"
-  value   = "v=spf1 -all"
+  content = "v=DKIM1; p="
   zone_id = var.cloudflare_zone_id
 }
 
-resource "cloudflare_record" "email_spoofing_prevention_2" {
+resource "cloudflare_dns_record" "prevent_email_spoofing_2" {
   name    = "rubenclaessens.nl"
   proxied = false
   ttl     = 1
   type    = "TXT"
-  value   = "v=DKIM1; p="
+  content = "v=spf1 -all"
   zone_id = var.cloudflare_zone_id
 }
 
-resource "cloudflare_record" "email_spoofing_prevention_3" {
+resource "cloudflare_dns_record" "prevent_email_spoofing_3" {
   name    = "rubenclaessens.nl"
   proxied = false
   ttl     = 1
   type    = "TXT"
-  value   = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s; rua=mailto:claessensruben2812@gmail.com"
+  content = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s; rua=mailto:claessensruben2812@gmail.com"
   zone_id = var.cloudflare_zone_id
 }
--- terraform/tunnel.tf
+++ terraform/tunnel.tf
@@ -3,35 +3,32 @@ resource "random_password" "tunnel_secret" {
   special = true
 }
 
-resource "cloudflare_tunnel" "hydro" {
-  account_id = var.cloudflare_account_id
-  name       = "hydro"
-  secret     = base64sha256(random_password.tunnel_secret.result)
+resource "cloudflare_zero_trust_tunnel_cloudflared" "rpi-01" {
+  account_id    = var.cloudflare_account_id
+  name          = "rpi-01"
+  tunnel_secret = base64sha256(random_password.tunnel_secret.result)
 }
 
-resource "cloudflare_tunnel_config" "hydro" {
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "rpi-01" {
   account_id = var.cloudflare_account_id
-  tunnel_id  = cloudflare_tunnel.hydro.id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.rpi-01.id
 
-  config {
-    ingress_rule {
+  config = {
+    ingress = [{
       hostname = "overseerr.${var.cloudflare_zone_name}"
       service  = "https://192.168.1.30"
-      origin_request {
+      origin_request = {
         no_tls_verify = true
       }
-    }
-
-    ingress_rule {
+      }, {
       hostname = "homeassistant.${var.cloudflare_zone_name}"
       service  = "https://192.168.1.30"
-      origin_request {
+      origin_request = {
         no_tls_verify = true
       }
-    }
-
-    ingress_rule {
-      service = "http_status:404"
-    }
+      },
+      {
+        service = "http_status:404"
+    }]
   }
 }
